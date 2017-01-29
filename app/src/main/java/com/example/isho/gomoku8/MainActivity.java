0ffce3d244360fb4f3ab5038931a1fbfb532430d
brangia <giatti@pdx.edu>
2017-01-28 20:47:32 -0800

    bg - test

--- app/src/main/java/com/example/isho/gomoku8/MainActivity.java
+++ app/src/main/java/com/example/isho/gomoku8/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     // commenting something. This is to test commit and push changes with ADS
+    // bg - test for push
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
