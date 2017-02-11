440eabd4a8bc7798b1b6cd4a8e9e9b5c26b58980
xingy <xingy@pdx.edu>
2017-02-11 13:50:01 -0800

    Added user setting variables and correctly check for detecting button toggles Added the variables to a Bundle object so they can be used within other activities. See BoardScreen class for example.

--- app/src/main/java/com/example/isho/gomoku8/ChooseStartOptions.java
+++ app/src/main/java/com/example/isho/gomoku8/ChooseStartOptions.java
@@ -3,18 +3,51 @@ package com.example.isho.gomoku8;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ToggleButton;
 import android.view.View;
+import android.util.Log;
 
 public class ChooseStartOptions extends AppCompatActivity {
+
+    String gameStyle = "Standard";
+    int broadSize = 10;
+    int playerSize = 1;
+    Intent intent;
+    String msg = "*******Log******* ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_start_options);
-        Intent intent = getIntent();
     }
+
     public void NewGame(View view){
+        ToggleButton style = (ToggleButton)findViewById(R.id.toggleButton3);
+        ToggleButton size  = (ToggleButton)findViewById(R.id.toggleButton2);
+        ToggleButton player = (ToggleButton)findViewById(R.id.toggleButton);
+
+        if(style.isChecked()) {
+            this.gameStyle = "FreeStyle";
+        }
+
+        if (size.isChecked()) {
+            this.broadSize = 20;
+        }
+
+        if (player.isChecked()) {
+            this.playerSize = 2;
+        }
+
+//        Log.d(msg, this.gameStyle);
+
         Intent intent = new Intent(this, BoardScreen.class);
-        intent.putExtra("size", 10);
+        Bundle bundle = new Bundle();
+        bundle.putString("gameStyle", this.gameStyle);
+        bundle.putInt("boardSize",this.broadSize);
+        bundle.putInt("playerSize",this.playerSize);
+        intent.putExtras(bundle);
+        //        Log.d(msg, this.gameStyle + this.broadSize + this.playerSize);
         startActivity(intent);
     }
+
 }
